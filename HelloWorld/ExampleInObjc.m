bd389c59f0cc4a1812c883610d7044e8d96da6c9
Dan Rosenstark <dan@dr2050.com>
2019-03-20 11:00:23 -0400

    Compiling with two variable

--- HelloWorld/ExampleInObjc.m
+++ HelloWorld/ExampleInObjc.m
@@ -1,6 +1,11 @@
 #import "ExampleInObjc.h"
 #import "HelloWorld-Swift.h"
 
+@interface ExampleInObjc()
+@property (nonatomic, strong) NSString *someString;
+@property (nonatomic, strong) NSString *someOtherString;
+@end
+
 @implementation ExampleInObjc
 
 + (void) doSomethingClass {}
@@ -8,6 +13,15 @@
 - (void) doSomething {
     ExampleInSwift *thing = [[ExampleInSwift alloc] init];
     NSLog(@"yes I have a swift class %@", thing);
+    NSLog(@"%@", _someString);
 }
+
+-(void)setSomeOtherString:(NSString *)someOtherString {
+    _someOtherString = someOtherString;
+}
+
+//-(NSString *)someOtherString {
+//    return _someOtherString;
+//}
 
 @end
