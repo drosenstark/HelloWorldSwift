26a1b5bc947216f488347295d6d2dd5e21bb6242
Dan Rosenstark <dan@dr2050.com>
2019-06-09 16:59:04 -0400

    yeah

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -1,8 +1,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let vc = AnotherVC()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         ExampleInSwift().doSomething(example: ExampleInObjc())
+        self.view.addSubview(vc.view)
+    }
+}
+
+class AnotherVC: UIViewController {
+    override func viewDidLoad() {
+        print("yes I actually did get called")
     }
 }
