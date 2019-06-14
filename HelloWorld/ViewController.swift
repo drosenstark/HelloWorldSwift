fe73b715b9b46d7ea7e911b746399285481be5a6
Dan Rosenstark <dan@dr2050.com>
2019-06-13 23:29:38 -0400

    Just practicing generics and interviewey stuff

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -1,17 +1,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let vc = AnotherVC()
-
     override func viewDidLoad() {
         super.viewDidLoad()
         ExampleInSwift().doSomething(example: ExampleInObjc())
-        self.view.addSubview(vc.view)
+        let asyncController = DispatchQueue.main.asyncAfterWithCancel(deadline: .now() + 1.0, execute: {
+            print("Yeah I'm ok")
+            return "Thing!"
+        } as ()->(String), handler: { thing in
+            print("Got it \(thing)")
+        })
+        asyncController.canceled = false
     }
 }
 
-class AnotherVC: UIViewController {
-    override func viewDidLoad() {
-        print("yes I actually did get called")
+extension DispatchQueue {
+    class AsyncController<T> {
+        var canceled = false
+        let handler: (T)->()
+        init(handler: @escaping (T)->()) {
+            self.handler = handler
+        }
     }
+    
+    func asyncAfterWithCancel<T>(deadline: DispatchTime, execute work: @escaping ()->(T), handler: @escaping (T)->()) -> AsyncController<T> {
+        let asyncController = AsyncController(handler: handler)
+        
+        asyncAfter(deadline: deadline) {
+            guard asyncController.canceled == false else { print("canceled!"); return }
+
+            let result = work()
+            handler(result)
+        }
+        return asyncController
+    }
+}
+
+enum Result<T, Error> {
+    case success(T)
+    case failure(Error)
 }
