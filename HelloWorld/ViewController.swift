3dbccce2884a304f84f93a8bf00afba752fc57ff
Dan Rosenstark <dan@dr2050.com>
2019-11-30 16:23:37 -0500

    Hmmm working on sub thing

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -5,8 +5,8 @@ class ViewController: UIViewController {
     @IBOutlet var collectionView: UICollectionView!
     // CRITICAL! If this is dealloc'ed the whole thing doesn't work
     var adapter: ListAdapter!
-
-    var strings = ["Foo", "Bar", "Biz", "Foo2", "Bar2", "Biz2"]
+    
+    var strings = ["Foo", "Bar", "Biz", "Child", "Foo2", "Bar2", "Biz2", "Foo3", "Bar3", "Biz3", "Foo4", "Bar4", "Biz4"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +26,10 @@ class ViewController: UIViewController {
             self.strings = self.strings.filter { !$0.contains("Bar") }
             self.adapter.performUpdates(animated: true, completion: nil)
         }
+
+        print("running through viewDidLoad")
+
+
     }
 }
 
@@ -34,12 +38,16 @@ extension ViewController: ListAdapterDataSource {
         return strings as [NSString]
     }
 
-    func listAdapter(_: ListAdapter, sectionControllerFor _: Any) -> ListSectionController {
-        let result = LabelSectionController()
-        if let flow = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
-            result.isHorizontal = flow.scrollDirection == .horizontal
+    func listAdapter(_: ListAdapter, sectionControllerFor object: Any) -> ListSectionController {
+        if let object = object as? String, object.starts(with: "Child") {
+            return OtherSectionController()
+        } else {
+            let result = LabelSectionController()
+            if let flow = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+                result.isHorizontal = flow.scrollDirection == .horizontal
+            }
+            return result
         }
-        return result
     }
 
     func emptyView(for _: ListAdapter) -> UIView? { return nil }
@@ -76,6 +84,55 @@ class LabelSectionController: ListSectionController {
             label.backgroundColor = .cyan
         }
         label.text = object
+        return cell
+    }
+
+    override func didUpdate(to object: Any) {
+        self.object = String(describing: object)
+    }
+}
+
+// MARK: - Second VC with CollectionView Inside
+
+class VC2: UIViewController {
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .magenta
+    }
+}
+
+class OtherSectionController: ListSectionController {
+    private var object: String?
+    var vc2: ViewController!
+
+    override init() {
+        let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
+        vc2 = storyboard.instantiateViewController(withIdentifier: "vc") as! ViewController
+        vc2.strings = vc2.strings.filter { !$0.contains("Child") }
+        let _ = vc2.view
+        if let flow = vc2.collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            flow.scrollDirection = .horizontal
+        }
+    }
+
+    override func numberOfItems() -> Int {
+        return 1
+    }
+
+    override func sizeForItem(at _: Int) -> CGSize {
+        let containerSize = collectionContext!.containerSize
+        return CGSize(width: containerSize.width, height: 400)
+    }
+
+    override func cellForItem(at index: Int) -> UICollectionViewCell {
+        guard let cell = collectionContext?.dequeueReusableCell(of: UICollectionViewCell.self, for: self, at: index) else {
+            fatalError()
+        }
+        // um why is it dequeueing from the other guys queue?
+        cell.subviews.forEach { $0.removeFromSuperview() }
+        cell.contentView.backgroundColor = .systemPink
+//        vc2.view.frame = cell.contentView.bounds
+//        cell.contentView.addSubview(vc2.view)
         return cell
     }
 
