5bd0d35480193167c2a16b73025dc19cfd7dafb7
Dan Rosenstark <dan@dr2050.com>
2019-12-01 14:17:55 -0500

    Works but disappears on touch

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,8 +8,8 @@ class ViewController: UIViewController {
     
     var strings = ["Foo", "Bar", "Biz", "Child", "Foo2", "Bar2", "Biz2", "Foo3", "Bar3", "Biz3", "Foo4", "Bar4", "Biz4"]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         let updater = ListAdapterUpdater()
         adapter = ListAdapter(updater: updater, viewController: self)
         adapter.collectionView = collectionView
@@ -19,17 +19,13 @@ class ViewController: UIViewController {
 //            flow.minimumInteritemSpacing = CGFloat(5.0)
 //            flow.minimumLineSpacing = CGFloat(5.0)
 //            collectionView.collectionViewLayout = flow
+//            flow.invalidateLayout()
 //        }
-//
-//
+
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
             self.strings = self.strings.filter { !$0.contains("Bar") }
             self.adapter.performUpdates(animated: true, completion: nil)
         }
-
-        print("running through viewDidLoad")
-
-
     }
 }
 
@@ -45,6 +41,7 @@ extension ViewController: ListAdapterDataSource {
             let result = LabelSectionController()
             if let flow = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
                 result.isHorizontal = flow.scrollDirection == .horizontal
+                print("is horiz \(result.isHorizontal)")
             }
             return result
         }
@@ -81,7 +78,7 @@ class LabelSectionController: ListSectionController {
             label = UILabel(frame: cell.contentView.bounds)
             label.textAlignment = .center
             cell.contentView.addSubview(label)
-            label.backgroundColor = .cyan
+            label.backgroundColor = isHorizontal ? .cyan : .systemPink
         }
         label.text = object
         return cell
@@ -103,16 +100,9 @@ class VC2: UIViewController {
 
 class OtherSectionController: ListSectionController {
     private var object: String?
-    var vc2: ViewController!
+//    var vc2: ViewController!
 
     override init() {
-        let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
-        vc2 = storyboard.instantiateViewController(withIdentifier: "vc") as! ViewController
-        vc2.strings = vc2.strings.filter { !$0.contains("Child") }
-        let _ = vc2.view
-        if let flow = vc2.collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
-            flow.scrollDirection = .horizontal
-        }
     }
 
     override func numberOfItems() -> Int {
@@ -121,7 +111,7 @@ class OtherSectionController: ListSectionController {
 
     override func sizeForItem(at _: Int) -> CGSize {
         let containerSize = collectionContext!.containerSize
-        return CGSize(width: containerSize.width, height: 400)
+        return CGSize(width: containerSize.width, height: 75)
     }
 
     override func cellForItem(at index: Int) -> UICollectionViewCell {
@@ -129,14 +119,29 @@ class OtherSectionController: ListSectionController {
             fatalError()
         }
         // um why is it dequeueing from the other guys queue?
-        cell.subviews.forEach { $0.removeFromSuperview() }
-        cell.contentView.backgroundColor = .systemPink
-//        vc2.view.frame = cell.contentView.bounds
-//        cell.contentView.addSubview(vc2.view)
+        cell.contentView.subviews.forEach { $0.removeFromSuperview() }
+        cell.contentView.backgroundColor = .orange
+        
+        let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
+        let vc2 = storyboard.instantiateViewController(withIdentifier: "vc") as! ViewController
+        vc2.strings = vc2.strings.filter { !$0.contains("Child") }
+        print("vc2 strings \(vc2.strings)")
+        let _ = vc2.view
+        if let flow = vc2.collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            flow.scrollDirection = .horizontal
+        }
+
+        cell.contentView.addSubview(vc2.view)
+
+        vc2.view.backgroundColor = .blue
+        print("vc2 superview \(vc2.view.superview) \(index)")
+        vc2.view.frame = cell.contentView.bounds
+        vc2.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         return cell
     }
 
     override func didUpdate(to object: Any) {
+        print("did update only on child right? \(object)")
         self.object = String(describing: object)
     }
 }
