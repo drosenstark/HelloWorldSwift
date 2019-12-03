8291f233df3d30cbee2629ebce11b6169a9ac65d
Dan Rosenstark <dan@dr2050.com>
2019-12-02 20:00:28 -0500

    GEtting there

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -6,10 +6,10 @@ class ViewController: UIViewController {
     // CRITICAL! If this is dealloc'ed the whole thing doesn't work
     var adapter: ListAdapter!
     var datasource = ThatAdapter()
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+
         if let flow = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
             flow.scrollDirection = .vertical
         }
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
 
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
             let source = self.datasource
-            
+
             source.strings = source.strings.filter { !$0.contains("What 8") }
             source.strings = source.strings.filter { !$0.contains("What 7") }
             source.strings = source.strings.filter { !$0.contains("What 10") }
@@ -31,13 +31,12 @@ class ViewController: UIViewController {
 }
 
 class ThatAdapter: NSObject, ListAdapterDataSource {
- 
     var isChild = false
 
-    lazy var strings:[String] = {
+    lazy var strings: [String] = {
         var strings = [String]()
-        for i in 0..<25 {
-            strings.append("What \(i+1)")
+        for i in 0 ..< 25 {
+            strings.append("What \(i + 1)")
         }
         if !isChild {
             strings.insert("Child", at: 3)
@@ -83,9 +82,9 @@ class LabelSectionController: ListSectionController {
 
     override init() {
         super.init()
-        self.inset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 10)
+        inset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
     }
-    
+
     override func numberOfItems() -> Int {
         return 1
     }
@@ -114,17 +113,16 @@ class LabelSectionController: ListSectionController {
     override func didUpdate(to object: Any) {
         self.object = String(describing: object)
     }
-    
+
     // MARK: - Needs Override
-    
+
     func typeForCell() -> UICollectionViewCell.Type {
         return UICollectionViewCell.self
     }
-    
+
     func backgroundColor() -> UIColor {
         return .systemPink
     }
-
 }
 
 class OtherSectionController: ListSectionController {
@@ -138,7 +136,7 @@ class OtherSectionController: ListSectionController {
         super.init()
         childDatasource.isChild = true
         let updater = ListAdapterUpdater()
-        childAdapter = ListAdapter(updater: updater, viewController: self.viewController!)
+        childAdapter = ListAdapter(updater: updater, viewController: viewController!)
         childAdapter.dataSource = childDatasource
     }
 
@@ -152,24 +150,11 @@ class OtherSectionController: ListSectionController {
     }
 
     override func cellForItem(at index: Int) -> UICollectionViewCell {
-        guard let cell = collectionContext?.dequeueReusableCell(of: AnotherCollectionCellView.self, for: self, at: index) else {
+        guard let cell = collectionContext?.dequeueReusableCell(of: CollectionViewHoldingCellView.self, for: self, at: index) as? CollectionViewHoldingCellView else {
             fatalError()
         }
-        // um why is it dequeueing from the other guys queue?
-        let collectionView: UICollectionView
 
-        if let oldCollectionView = cell.contentView.subviews.first as? UICollectionView {
-            collectionView = oldCollectionView
-        } else {
-            let flow = UICollectionViewFlowLayout()
-            flow.scrollDirection = .horizontal
-            collectionView = UICollectionView(frame: cell.contentView.bounds, collectionViewLayout: flow)
-            cell.contentView.backgroundColor = .orange
-            collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-            cell.contentView.addSubview(collectionView)
-        }
-
-        childAdapter.collectionView = collectionView
+        childAdapter.collectionView = cell.collectionView
 
         return cell
     }
@@ -180,10 +165,22 @@ class OtherSectionController: ListSectionController {
     }
 }
 
-class AnotherCollectionCellView: UICollectionViewCell {
-    
+class CollectionViewHoldingCellView: UICollectionViewCell {
+    var collectionView: UICollectionView!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        let flow = UICollectionViewFlowLayout()
+        flow.scrollDirection = .horizontal
+        collectionView = UICollectionView(frame: contentView.bounds, collectionViewLayout: flow)
+        contentView.backgroundColor = .orange
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        contentView.addSubview(collectionView)
+    }
+
+    required init?(coder _: NSCoder) {
+        fatalError("not implemented")
+    }
 }
 
-class HorizCollectionCellView: UICollectionViewCell {
-    
-}
+class HorizCollectionCellView: UICollectionViewCell {}
